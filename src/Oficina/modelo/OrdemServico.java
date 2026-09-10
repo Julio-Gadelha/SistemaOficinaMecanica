a87d08639de4f05dc75e5317641952fffae773e5
julio gabriel <juliogabriel78956@gmail.com>
2026-09-10 13:01:39 -0300

    Modelo

--- src/Oficina/modelo/OrdemServico.java
+++ src/Oficina/modelo/OrdemServico.java
@@ -8,25 +8,29 @@ public abstract class OrdemServico {
     private LocalDate entradaCliente;
     private LocalDate dataPrevistaEntrega;
     private TipoVeiculo tipoVeiculo;
+    private double valorpecas;
     public static int ordemServico = 0;
 
-    public OrdemServico(String nomeCliente, String telefoneCliente, TipoVeiculo tipoVeiculo) throws TelefoneInvalidoException {
+    public OrdemServico(String nomeCliente, String telefoneCliente, double valorpecas,TipoVeiculo tipoVeiculo) throws TelefoneInvalidoException {
         if (telefoneCliente == null || telefoneCliente.trim().isEmpty() || telefoneCliente.trim().length() < 11) {
             throw new TelefoneInvalidoException("Telefone inválido: não pode ser vazio e deve ter pelo menos 11 dígitos");
         }
-
         this.nomeCliente = nomeCliente;
         this.telefoneCliente = telefoneCliente;
         this.tipoVeiculo = tipoVeiculo;
+        this.valorpecas = valorpecas;
         this.entradaCliente = LocalDate.now();
         this.dataPrevistaEntrega = this.entradaCliente.plusDays(getDiasParaEntrega());
         ordemServico ++;
-
     }
 
     public abstract int getDiasParaEntrega();
 
-    public abstract double valorTotalObra(double valorpecas);
+    public abstract double valorTotalObra();
+
+    public double getValorpecas() {
+        return valorpecas;
+    }
 
     public String getNomeCliente() {
         return nomeCliente;
@@ -61,6 +65,8 @@ public abstract class OrdemServico {
                 "Entrega Cliente: " + entradaCliente + "\n" +
                 "Entrega Veiculo: " + dataPrevistaEntrega + "\n" +
                 "Quantidade veiculo: " + ordemServico + "\n" +
+                "Total a pagar : " + valorTotalObra()+ "\n" +
                 "Dias Entrega: " + getDiasParaEntrega() + "\n" ;
+
     }
 }
--- src/Oficina/modelo/ProdutosImportado.java
+++ src/Oficina/modelo/ProdutosImportado.java
@@ -2,5 +2,6 @@ package Oficina.modelo;
 
 public interface ProdutosImportado {
 
-     double  solicitarPecaImportada( double valorPecas);
+     void solicitarPecasImportadas();
+
 }
